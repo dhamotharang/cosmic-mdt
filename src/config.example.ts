1526f960710b7d8b9d6bab100decc74aa804b9a5
WhitigolProd <36452182+WhitigolProd@users.noreply.github.com>
2021-08-22 18:43:49 -0400

    Update config.example.ts

--- src/config.example.ts
+++ src/config.example.ts
@@ -7,11 +7,11 @@ const config = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "admin",
-  databaseName: process.env.DB_NAME || "snaily-cad",
+  databaseName: process.env.DB_NAME || "cosmic-mdt",
   jwtSecret: process.env.JWT_SECRET || "bongo super cat",
   env: process.env.PROFILE || "production",
-  allowIframes: false, // true or false
-  secureCookie: false, // oNLY SET TO `true` if your site uses `https://`!
+  allowIframes: false, // true or false | Known to cause errors, do not use if you don't know what you're doing.
+  secureCookie: false, // oNLY SET TO `true` if your site uses `https://`! | Known to cause errors, do not use if you don't know what you're doing.
   databasePort: 3306,
 };
 
